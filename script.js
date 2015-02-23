4327f9f4e474a80b23c7d051449b7e485196b420
mrjimmoore <mrjimmoore@outlook.com>
2015-02-22 20:31:10 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,43 +1,46 @@
 // module
 
-var app = angular.module('myApp', ['ngRoute']);
+var mainApp = angular.module('mainApp', ['ngRoute']);
 
 // routes
 
-app.config(function ($routeProvider) {
+mainApp.config(function ($routeProvider, $locationProvider) {
     $routeProvider
         .when('/', { templateUrl: 'home.html', controller: 'homeController' })
-        .when('/about', { templateUrl: 'about.html', controller: 'aboutController' })
+        .when('/about', { templateUrl: '/about.html', controller: 'aboutController' })
         .when('/help', { templateUrl: 'help.html', controller: 'helpController' })
         .when('/addContact', { templateUrl: 'contact.html', controller: 'addContactController' })
         .when('/listContacts', { templateUrl: 'contacts.html', controller: 'listContactsController' })
         .when('/editContact/:_id', { templateUrl: 'contact.html', controller: 'editContactController' })
         .otherwise({ templateUrl: 'home.html', controller: 'homeController' });
+
+    $locationProvider.html5Mode(true);
+    //$locationProvider.hashPrefix('!')
 });
 
 // controllers
 
-app.controller('mainController', function ($scope) {
+mainApp.controller('mainController', function ($scope) {
     $scope.bannerTitle = 'SPA Example';
     $scope.bannerSubTitle = 'using HTML5, AngularJS, Node.js, and MongoDB';
     $scope.pageTitle = 'Contacts';
-    $scope.pageMessage = 'Lorem ipsum potenti odio imperdiet tempor pulvinar metus, per imperdiet nulla mauris pharetra elementum orci, erat lectus bibendum et himenaeos platea interdum turpis elementum nibh interdum rutrum sagittis congue dolor blandit, condimentum mattis imperdiet mi nulla habitasse pellentesque aliquam, sociosqu magna ultricies nam quis phasellus fusce ultrices.';
+    $scope.pageMessage = 'Morbi ullamcorper auctor convallis quam turpis molestie eget sem, leo cras velit lacus vulputate imperdiet molestie, gravida suscipit facilisis sagittis per fusce ante.';
     $scope.currentDate = new Date();
 });
 
-app.controller('homeController', function ($scope) {
+mainApp.controller('homeController', function ($scope) {
     $scope.message = 'Home page message...';
 });
 
-app.controller('aboutController', function ($scope) {
+mainApp.controller('aboutController', function ($scope) {
     $scope.message = 'About page message...';
 });
 
-app.controller('helpController', function ($scope) {
+mainApp.controller('helpController', function ($scope) {
     $scope.message = 'Help page message...';
 });
 
-app.controller('addContactController', function ($scope) {
+mainApp.controller('addContactController', function ($scope) {
     $scope.message = 'Add New Contact'
 
     $scope.saveContact = function() {
@@ -45,13 +48,14 @@ app.controller('addContactController', function ($scope) {
     };
 });
 
-app.controller('listContactsController', function ($scope, $http) {
+mainApp.controller('listContactsController', function ($scope, $http) {
     $http.get('http://localhost:3000/contact')
         .success(function(data, status, headers, config, statusText) { $scope.contacts = data; })
         .error(function(data, status, headers, config, statusText) { alert('Error executing http get collection.'); }) ;
+
 });
 
-app.controller('editContactController', function ($scope, $http, $routeParams) {
+mainApp.controller('editContactController', function ($scope, $http, $routeParams) {
     $http.get('http://localhost:3000/contact/' + $routeParams._id)
         .success(function(data, status, headers, config, statusText) { alert(data); $scope.contact = data; })
         .error(function(data, status, headers, config, statusText) { alert('Error executing http get document.'); }) ;
