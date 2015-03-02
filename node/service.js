06eb175a01ac10d36337c32aac5f21b15be8ed01
Jim <mrjimmoore@outlook.com>
2015-03-02 12:02:39 -0500

    Committing changes...

--- node/service.js
+++ node/service.js
@@ -1,7 +1,7 @@
 var restify = require('restify');
 var mongojs = require('mongojs');
 var db = mongojs('myContacts', ['contact']);
-var server = restify.createServer({ name: 'ContactsRestApi' });
+var server = restify.createServer({name: 'ContactsRestApi'});
 
 server.use(restify.acceptParser(server.acceptable));
 server.use(restify.queryParser());
@@ -10,15 +10,15 @@ server.use(restify.fullResponse());
 
 server.get('/contacts', function (req, res, next) {
     db.contact.find(function (err, data) {
-        res.writeHead(200, { 'Content-Type': 'application/json; charset=utf-8' });
+        res.writeHead(200, {'Content-Type': 'application/json; charset=utf-8'});
         res.end(JSON.stringify(data));
     });
     return next();
 });
 
 server.get('/contacts/:_id', function (req, res, next) {
-    db.contact.findOne({ _id: mongojs.ObjectId(req.params._id) }, function (err, data) {
-        res.writeHead(200, { 'Content-Type': 'application/json; charset=utf-8' });
+    db.contact.findOne({_id: mongojs.ObjectId(req.params._id)}, function (err, data) {
+        res.writeHead(200, {'Content-Type': 'application/json; charset=utf-8'});
         res.end(JSON.stringify(data));
     });
     return next();
@@ -26,14 +26,14 @@ server.get('/contacts/:_id', function (req, res, next) {
 
 server.post('/contacts', function (req, res, next) {
     db.contact.save(req.params, function (err, data) {
-        res.writeHead(200, { 'Content-Type': 'application/json; charset=utf-8' });
+        res.writeHead(200, {'Content-Type': 'application/json; charset=utf-8'});
         res.end(JSON.stringify(data));
     });
     return next();
 });
 
 server.put('/contacts/:_id', function (req, res, next) {
-    db.contact.findOne({ _id: mongojs.ObjectId(req.params._id) }, function (err, data) {
+    db.contact.findOne({_id: mongojs.ObjectId(req.params._id)}, function (err, data) {
         var updProd = {};
         for (var n in data) {
             updProd[n] = data[n];
@@ -41,8 +41,8 @@ server.put('/contacts/:_id', function (req, res, next) {
         for (var n in req.params) {
             updProd[n] = req.params[n];
         }
-        db.contact.update({ _id: mongojs.ObjectId(req.params._id) }, updProd, { multi: false }, function (err, data) {
-            res.writeHead(200, { 'Content-Type': 'application/json; charset=utf-8' });
+        db.contact.update({_id: mongojs.ObjectId(req.params._id)}, updProd, {multi: false}, function (err, data) {
+            res.writeHead(200, {'Content-Type': 'application/json; charset=utf-8'});
             res.end(JSON.stringify(data));
         });
     });
@@ -50,8 +50,8 @@ server.put('/contacts/:_id', function (req, res, next) {
 });
 
 server.del('/contacts/:_id', function (req, res, next) {
-    db.contact.remove({ _id: mongojs.ObjectId(req.params._id) }, function (err, data) {
-        res.writeHead(200, { 'Content-Type': 'application/json; charset=utf-8' });
+    db.contact.remove({_id: mongojs.ObjectId(req.params._id)}, function (err, data) {
+        res.writeHead(200, {'Content-Type': 'application/json; charset=utf-8'});
         res.end(JSON.stringify(true));
     });
     return next();
