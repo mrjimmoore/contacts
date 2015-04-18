2e5c16b608e161e8beb58880de5bf4017b6c1e60
mrjimmoore <mrjimmoore@outlook.com>
2015-04-18 19:14:34 -0400

    Committing changes

---
+++ controllers.js
@@ -0,0 +1,25 @@
+app.controller("mainController", function ($scope) {
+    $scope.copyrightDate = new Date();
+
+    //// alternative to templateUrl by allowing the routeProvider to be loaded via ng-click.
+    //// requires $locatoin injection.
+    //$scope.loadView = function (uri) {
+    //    $location.path(uri);
+    //}
+});
+
+app.controller("homeController", function ($scope) {
+    $("#headerContent").load("header.html");
+});
+
+app.controller("aboutController", function ($scope) {
+    $("#headerContent").load("header.html");
+});
+
+app.controller("helpController", function ($scope) {
+    $("#headerContent").load("header.html");
+});
+
+app.controller("settingsController", function ($scope) {
+    $("#headerContent").load("header.html");
+});
